54e136725fdd49e219322b8f4787652742c8f936
bala-amypo <bala@amypo.in>
2025-12-17 05:14:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/entity.java
@@ -0,0 +1,21 @@
+public class student{
+    private int id;
+    private String name;
+    private float cgpa;
+public int getId(){
+    return id;
+}
+public void setId(int Id){
+    thid.Id=Id;
+}
+public String getname(){
+    return name;
+}
+public void setname(String name){
+    this.name=name;
+}
+public float getcgpa(){
+    return cgpa;
+}
+
+}
